5db3db0c6fe1fbcb927682c7af7b407bc988bd13
Кирилл Попов <diplomatgmg@gmail.com>
2024-03-13 23:14:50 +0300

    Таски хранятся в localStorage

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App/App'
 import { Provider } from 'react-redux'
-import store from './redux/store'
+import store, { persistor } from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as Element
 )
 root.render(
-  <Provider store={store}>
-    <App/>
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App/>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 )
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,41 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import taskReducer from './taskSlice'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-const store = configureStore({
-  reducer: {
-    tasks: taskReducer
-  }
+const rootReducer = combineReducers({
+  tasks: taskReducer
 })
 
+const persistConfig = {
+  key: 'root',
+  storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
+})
+
+const persistor = persistStore(store)
+
+export { persistor }
 export default store
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
